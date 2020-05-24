7a44457022cd498ef037b080f92058b1976bbd22
Bartek <Bartek@MBP-Bartek.panda.home>
2020-05-24 11:11:22 +0200

    Add RootQuery - books

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
-  GraphQLList
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull
 } = require("graphql");
 const app = express();
 
@@ -27,16 +29,57 @@ const books = [
 ];
 // END - DATE
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "HelloWorld",
-    fields: () => ({
-      message: {
-        type: GraphQLString,
-        resolve: () => "Hello World"
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "This represents a book written by an author",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
+    author: {
+      type: AuthorType,
+      resolve: book => {
+        return authors.find(author => author.id === book.authorId);
       }
-    })
+    }
   })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "This represents a author of a book",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) }
+  })
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: "List of All Books",
+      resolve: () => books
+    }
+  })
+});
+
+// const schema = new GraphQLSchema({
+//   query: new GraphQLObjectType({
+//     name: "HelloWorld",
+//     fields: () => ({
+//       message: {
+//         type: GraphQLString,
+//         resolve: () => "Hello World"
+//       }
+//     })
+//   })
+// });
+
+const schema = new GraphQLSchema({
+  query: RootQueryType
 });
 
 app.use(
