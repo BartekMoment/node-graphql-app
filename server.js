f6955f203c206d1611d34e503660d872b4a1d7f3
Bartek <Bartek@MBP-Bartek.panda.home>
2020-05-24 10:59:43 +0200

    Add Date JSON

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const expressGraphQL = require("express-graphql");
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList
+} = require("graphql");
 const app = express();
+
+// DATE
+const authors = [
+  { id: 1, name: "J. K. Rowling" },
+  { id: 2, name: "J. R. R. Tolkien" },
+  { id: 3, name: "Brent Weeks" }
+];
+
+const books = [
+  { id: 1, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
+  { id: 2, name: "Harry Potter and the Prisoner of Azkaban", authorId: 1 },
+  { id: 3, name: "Harry Potter and the Goblet of Fire", authorId: 1 },
+  { id: 4, name: "The Fellowship of the Ring", authorId: 2 },
+  { id: 5, name: "The Two Towers", authorId: 2 },
+  { id: 6, name: "The Return of the King", authorId: 2 },
+  { id: 7, name: "The Way of Shadows", authorId: 3 },
+  { id: 8, name: "Beyond the Shadows", authorId: 3 }
+];
+// END - DATE
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
-    name: "Hello World",
+    name: "HelloWorld",
     fields: () => ({
       message: {
         type: GraphQLString,
