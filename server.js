344118839ac07f273bb4712b83f5c3bb1e2eaa97
Bartek <Bartek@MBP-Bartek.panda.home>
2020-05-24 10:37:53 +0200

    Add GraphQL Express - server.js

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const expressGraphQL = require("express-graphql");
 const app = express();
 
+app.use(
+  "/graphql",
+  expressGraphQL({
+    graphiql: true
+  })
+);
 app.listen(5000, () => console.log("Server Running!"));
