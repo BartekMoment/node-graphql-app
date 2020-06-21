2d0bd5b1678cffa768f65071c633a583518aa250
Bartek <Bartek@MBP-Bartek.panda.home>
2020-06-21 18:24:45 +0200

    Add Mutation Root - AddAuthor field

--- server.js
+++ server.js
@@ -12,20 +12,20 @@ const app = express();
 
 // DATE
 const authors = [
-  { id: 0, name: "J. K. Rowling" },
-  { id: 1, name: "J. R. R. Tolkien" },
-  { id: 2, name: "Brent Weeks" }
+  { id: 1, name: "J. K. Rowling" },
+  { id: 2, name: "J. R. R. Tolkien" },
+  { id: 3, name: "Brent Weeks" }
 ];
 
 const books = [
-  { id: 0, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
-  { id: 1, name: "Harry Potter and the Prisoner of Azkaban", authorId: 1 },
-  { id: 2, name: "Harry Potter and the Goblet of Fire", authorId: 1 },
-  { id: 3, name: "The Fellowship of the Ring", authorId: 2 },
-  { id: 4, name: "The Two Towers", authorId: 2 },
-  { id: 5, name: "The Return of the King", authorId: 2 },
-  { id: 6, name: "The Way of Shadows", authorId: 3 },
-  { id: 7, name: "Beyond the Shadows", authorId: 3 }
+  { id: 1, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
+  { id: 2, name: "Harry Potter and the Prisoner of Azkaban", authorId: 1 },
+  { id: 3, name: "Harry Potter and the Goblet of Fire", authorId: 1 },
+  { id: 4, name: "The Fellowship of the Ring", authorId: 2 },
+  { id: 5, name: "The Two Towers", authorId: 2 },
+  { id: 6, name: "The Return of the King", authorId: 2 },
+  { id: 7, name: "The Way of Shadows", authorId: 3 },
+  { id: 8, name: "Beyond the Shadows", authorId: 3 }
 ];
 // END - DATE
 
@@ -111,6 +111,21 @@ const RootMutationType = new GraphQLObjectType({
         };
         books.push(book);
         return book;
+      }
+    },
+    addAuthor: {
+      type: AuthorType,
+      description: "Add an author",
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) }
+      },
+      resolve: (parent, args) => {
+        const author = {
+          id: authors.length + 1,
+          name: args.name
+        };
+        authors.push(author);
+        return author;
       }
     }
   })
